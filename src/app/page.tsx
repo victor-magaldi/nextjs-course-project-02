a70c271ed7c6e215247cac11cb4b2b2b4cc97742
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-10-20 20:09:55 -0300

    styles: add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   return (
     <div>
-      Home
-      <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippet</h1>
+        <Link href={"/snippets/new"} className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+
+      <div className="flex flex-col gap-2">
         {snippets.map((snippet) => {
-          return <div key={snippet.id}>{snippet.title}</div>;
+          const { id, title } = snippet;
+          return (
+            <Link
+              key={id}
+              href={`/snippets/${id}`}
+              className="flex justify-between items-center p-2 border rounded"
+            >
+              <div>{title}</div>
+              <div>View</div>
+            </Link>
+          );
         })}
       </div>
     </div>
