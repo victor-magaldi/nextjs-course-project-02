c9d3f4aea67eb07d3e8579e37d5b137fbebdc587
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-10-20 21:23:39 -0300

    feat: add Monaco Editor in SnippetEditForm

--- src/components/SnippetEditForm.tsx
+++ src/components/SnippetEditForm.tsx
@@ -1,10 +1,30 @@
 "use client";
 import type { Snippet } from "@prisma/client";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  return <div>Client Component has Snippet {snippet.title}</div>;
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  console.log(code);
+
+  return (
+    <div>
+      Client Component has Snippet {snippet.title}
+      <Editor
+        height="40vh"
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
 }
