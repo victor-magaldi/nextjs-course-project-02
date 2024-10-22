b403fd65d5cad8ed04af0047ae23c434cbc9fffc
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-10-21 22:08:34 -0300

    feat: add SSG with generateStaticParams

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -52,3 +52,13 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </div>
   );
 }
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    };
+  });
+}
